90c6f654df8953b504c16b0350c30c993d1d994b
Andrew Seward <awswrd@gmail.com>
2020-05-25 14:28:37 -0700

    Get an MVP working

--- src/heroku_database_url_splitter.nim
+++ src/heroku_database_url_splitter.nim
@@ -1,25 +1,42 @@
 import os
 import strutils
+import uri
 
-
-const exampleUrl = block:
-  const login = "drBrule123"
+static:
+  const scheme = "postgres"
+  const username = "drBrule123"
   const password = "Pa55w0rD!"
   const host = "example.com"
   const port = 5432
   const dbName = "my_cool_app_production"
 
-  "postgres://$1:$2@$3:$4/$5" % [
-    login,
-    password,
-    host,
-    $port,
-    dbName,
-  ]
+  const exampleUrl =
+    "$1://$2:$3@$4:$5/$6" % [scheme, username, password, host, $port, dbName]
 
-static:
   assert exampleUrl ==
     "postgres://drBrule123:Pa55w0rD!@example.com:5432/my_cool_app_production"
 
+  let parsed = parseUri exampleUrl
+
+  assert parsed.scheme == scheme
+  assert parsed.username == username
+  assert parsed.password == password
+  assert parsed.hostname == host
+  assert parseInt(parsed.port) == port
+  assert parsed.path.strip(chars = {'/'}) == dbName
+
 when isMainModule:
-  echo("Hello, World!")
+  let databaseUrl = getEnv "DATABASE_URL"
+  let parsed = parseUri databaseUrl
+  echo """
+export DATABASE_HOST='$1'
+export DATABASE_PORT='$2'
+export DATABASE_LOGIN='$3'
+export DATABASE_PASSWORD='$4'
+export DATABASE_NAME='$5'""" % [
+    parsed.hostname,
+    parsed.port,
+    parsed.username,
+    parsed.password,
+    parsed.path.strip(chars = {'/'})
+  ]
